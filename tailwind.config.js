3be84143dbe4fb5d74ff610932b5273dd183763e
galactic_ <ndg72609@gmail.com>
2023-03-11 20:17:56 -0800

    ok i did it lool

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: { padding: { "100px": "25rem", "56px":"14rem", "20px":"5rem" } },
+  },
+  plugins: [],
+};
